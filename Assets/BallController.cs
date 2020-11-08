2ead1e560690b82ea5a542fa7ec7ac83d199df41
yusuke-07 <altair07ysk@gmail.com>
2020-11-08 19:54:07 +0900

    「Score」Text位置の調整
    Anchorsの値を調整し、ゲームビューの画面サイズを変えても右上に表示するようにした。
    また、「ScoreAddition」スクリプトの内容を「BallController」スクリプトの方に追記し、「ScoreAddition」スクリプトを削除した。

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,6 +5,24 @@ using UnityEngine.UI;
 public class BallController : MonoBehaviour
 {
 
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    // デフォルトの点数
+    private int score = 0;
+
+    // SmallStarの得点
+    private int smallstar = 10;
+
+    // LargeStarの得点
+    private int largestar = 20;
+
+    // SmallCloudの得点
+    private int smallcloud = 30;
+
+    // LargeCloudの得点
+    private int largecloud = 50;
+
     //ボールが見える可能性のあるz軸の最大値
     private float visiblePosZ = -6.5f;
 
@@ -14,6 +32,9 @@ public class BallController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        //シーン中のScoreオブジェクトを取得
+        this.scoreText = GameObject.Find("Score");
+
         //シーン中のGameOverTextオブジェクトを取得
         this.gameoverText = GameObject.Find("GameOverText");
     }
@@ -28,4 +49,35 @@ public class BallController : MonoBehaviour
             this.gameoverText.GetComponent<Text>().text = "Game Over";
         }
     }
+
+    // 衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+
+        // 衝突対象に応じて得点を加算
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.score += this.smallstar;
+        }
+
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += this.largestar;
+        }
+
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.score += this.smallcloud;
+        }
+
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.score += this.largecloud;
+        }
+
+        // 得点を表示
+        this.scoreText.GetComponent<Text>().text = this.score.ToString();
+
+    }
+
 }
